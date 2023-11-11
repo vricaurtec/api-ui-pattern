3863fc07aad73137ce04bfc6a214210cd1086c53
vricaurtec <vricaurte842@gmail.com>
2023-11-11 15:51:42 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { Carousel, Spinner } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const SlideShow = () => {
+  const [catFacts, setCatFacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
+  useEffect(() => {
+    // Fetch cat facts from the API
+    const fetchCatFacts = async () => {
+      try {
+        const response = await fetch("https://catfact.ninja/facts?limit=5");
+        const data = await response.json();
+        setCatFacts(data.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching cat facts:", error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchCatFacts();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="load">
+        <Spinner animation="border" variant="primary" />
+        <p className="mt-2">Loading...</p>
+      </div>
+    );
+  }
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      {catFacts.map((fact, i) => (
+        <Carousel.Item key={i}>
+          <img
+            className="pic"
+            src={`https://placekitten.com/900/400?image=${i + 1}`}
+            alt={`Slide ${i + 1}`}
+          />
+          <Carousel.Caption>
+            <h3>Did you know?</h3>
+            <p>{fact.fact}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cat Fact Slideshow</h1>
+      <SlideShow />
     </div>
   );
 }
